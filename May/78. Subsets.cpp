0e57648a2522a4ab732d441bb6f787879ae12e00
Piyush Tailor <piyushtailor4@gmail.com>
2024-05-21 12:35:53 +0530

    Day1

---
+++ May/78. Subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void findallsubsets(int index, vector<int> nums, vector<int> &subset,
+                   vector<vector<int>> &result) {
+        if (index>=nums.size()){
+            result.push_back(subset);
+          
+            return;
+        }
+        subset.push_back(nums[index]);
+        findallsubsets(index+1,nums,subset,result);
+        subset.pop_back();
+        findallsubsets(index+1,nums,subset,result);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> subset;
+
+        findallsubsets(0, nums, subset, result);
+        return result;
+    }
+};
